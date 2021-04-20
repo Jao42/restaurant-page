25a84d1790e56d0171ac1db12d9400916e50600d
Jao42 <joaovictormedeiros41@gmail.com>
2021-04-20 09:51:25 -0300

    setting menu links

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("  function createDefaultFigures(place, image, alt, caption) {\n\n    const homeDivFigure = document.createElement('div');\n    const homeFigure = document.createElement('figure');\n    const homeFigureImg = document.createElement('img');\n    const homeFigCaption = document.createElement('figcaption');\n\n    homeDivFigure.classList.add('home-image-div');\n    homeFigure.classList.add('home-image');\n    homeFigCaption.classList.add('default-figcaption');\n\n    homeFigureImg.setAttribute('src', image);\n    homeFigureImg.setAttribute('alt', alt);\n    homeFigCaption.innerHTML = caption;\n\n    homeDivFigure.appendChild(homeFigure);\n    homeFigure.appendChild(homeFigureImg);\n    homeFigure.appendChild(homeFigCaption);\n\n    place.appendChild(homeDivFigure);\n    return 0;\n  } \n\n\nconst pageContent = document.querySelector('.home-content');\nconst menuButton = document.querySelector('.menu-button');\n\ncreateDefaultFigures(\n    pageContent,\n    './imgs/restaurant4-red.jpg', \n    'Restaurant lounge',\n    'Ricco Ristourante<br>for over 10,000 years making the most expensive food in Italy')\n\ncreateDefaultFigures(\n    pageContent,\n    './imgs/restaurant-red.jpg', \n    'A restaurant Chief', \n    'With incredibly well-paid chefs');\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
